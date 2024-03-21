40a1631b87ca24b952b09656e2da58880be7625e
carlosnunezbaro <127980966+carlosnunezbaro@users.noreply.github.com>
2024-03-21 18:41:06 +0100

    lockers.c

--- source/lockers.c
+++ source/lockers.c
@@ -40,5 +40,129 @@ void guardarModificaciones(tLockers *lock, int numLockers) {
                 lock[i].Poblacion, lock[i].Compartimentos_totales, lock[i].Compartimentos_ocupados);
     }
 
+
+
+//cortado
+
+#include "lockers.h"
+#include "clientes.h"
+void vaciarl(char temp[]);
+void mostrarMenu();
+void buscarLocker(tLockers *lock, int numLockers);
+void copiarl(char temp[],int i);
+
+    int i, j;
+    char temp[100];
+    char aux;
+    int cont = 0;
+    FILE *f;
+    f = fopen("Lockers.txt","r");
+    if(f==NULL){
+        printf("no se ha encontrado el archivo.\n");
+        exit(1);
+    }
+    while(!feof(f)){
+       char temp[100];
+       fgets(temp,100,f);
+      cont++;
+    }
+    rewind(f);
+
+    tLockers *lock;
+    lock = (tLockers*)malloc(cont * sizeof(tLockers));
+    if (lock == NULL){
+        printf("no se ha podido reservar la memoria.\n");
+        exit(1);
+    }
+    for(i = 0; !feof(f); i++) {
+        char temp[100];
+        vaciarl(temp);
+        char aux = '0';
+        for(j = 0; aux != '-'; j++) {
+            aux = fgetc(f);
+            if(aux != '-') {
+                temp[j] = aux;
+            }
+        }
+        copiarl(temp, i);
+        fgets(temp, 100, f);
+        printf("ID %s\n", lock[i].ID_Locker);
+    }
+    mostrarMenu();
+
+    for (i = 0; i < cont; i++) {
+        free(lock[i].ID_Locker);
+    }
+    free(lock);
+
+    return 0;
+}
+
+void mostrarMenu(tLockers *lock, int numLockers,int cont) { 
+    int opcion;
+    do {
+        printf("\n---- MENU ----\n");
+        printf("1. Buscar un locker por ID\n");
+        printf("2. Modificar información de un locker\n");
+        printf("3. Guardar modificaciones\n");
+        printf("4. Salir\n");
+        printf("Ingrese una opción: ");
+        scanf("%d", &opcion);
+
+        switch (opcion) {
+            case 1:
+                buscarLocker(lock, cont);
+                break;
+            case 2:
+                modificarInformacion(lock, cont);
+                break;
+            case 3:
+                guardarModificaciones(lock, cont);
+                break;
+            case 4:
+                printf("Saliendo del programa...\n");
+                break;
+            default:
+                printf("Opción no válida. Por favor, ingrese una opción válida.\n");
+        }
+    } while (opcion != 4);
+}
+
+void buscarLocker(tLockers *lock, int numLockers) {   //busca el locker segun el ID y muestra sus datos por pantalla
+    char idBuscar[IDLK];
+    printf("Ingrese el ID del locker que desea buscar: ");
+    scanf("%s", idBuscar);
+
+    for (i = 0; i < numLockers; i++) {
+        if (strcmp(lock[i].ID_Locker, idBuscar) == 0) {
+            printf("Locker encontrado:\n");
+            printf("ID: %s\n", lock[i].ID_Locker);
+            printf("Ubicacion: %s\n", lock[i].Ubicacion);
+            printf("Provincia: %s\n", lock[i].Provincia);
+            printf("Poblacion: %s\n", lock[i].Poblacion);
+            printf("Compartimentos totales: %s\n", lock[i].Compartimentos_totales);
+            printf("Compartimentos ocupados: %s\n", lock[i].Compartimentos_ocupados);
+            return;
+        }
+    }
+    printf("Locker con ID %s no encontrado.\n", idBuscar);
+}
+
+
+void vaciarl(char temp[]) { //vacia la maemoria de la variable temporal i
+    for(i = 0; i < 100; i++) {
+        temp[i] = '\0';
+    }
+}
+void copiarl(char temp[], int i){ //
+    int N = strlen(temp) + 1;
+    lock[i].ID_Locker = (char*)malloc(N * sizeof(char));
+    if (lock[i].ID_Locker == NULL) {
+        printf("No se ha podido reservar la memoria.\n");
+        exit(1);
+    }
+    strcpy(lock[i].ID_Locker, temp);
+    
+
     fclose(f);
 }
